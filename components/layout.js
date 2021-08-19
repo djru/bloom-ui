e8d84fac1339f48ecd979963026ab617ab5ccce1
Daniel Ruswick <danielruswick@Daniels-MacBook-Air.local>
2021-08-19 16:41:47 -0500

    ready for vercel

---
+++ components/layout.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+
+export default function Layout({ children }) {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch("https://bloom-health.herokuapp.com/whoami")
+      .then((r) => r.json())
+      .then((r) => {
+        console.log(r);
+        setUser(r);
+      })
+      .catch((r) => {
+        console.error(r);
+      });
+  }, []);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Bloom - BP Tracker</title>
+        <meta name="description" content="Bloom blood pressure tracker" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className={styles.nav}>
+        <h1>
+          <Link href="/">🌸 Bloom Health</Link>
+        </h1>
+
+        <div className={styles.links}>
+          {!user ? (
+            <Link href="/login">
+              <a>Log In</a>
+            </Link>
+          ) : null}
+        </div>
+      </nav>
+
+      <main className={styles.main}>{children}</main>
+
+      <footer className={styles.footer}>
+        © Dan Ruswick {new Date().getFullYear()}
+      </footer>
+    </div>
+  );
+}
---
+++ pages/login/index.js
@@ -0,0 +1,23 @@
+import Layout from "../../components/layout";
+import styles from "../../styles/Login.module.css";
+import { useEffect, useState } from "next";
+
+export default function Login() {
+  return (
+    <Layout>
+      <form className={styles.form}>
+        <input
+          type="email"
+          placeholder="email"
+          className={styles.inputField}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.inputField}
+        ></input>
+        <button className={styles.submit}>Log In</button>
+      </form>
+    </Layout>
+  );
+}
