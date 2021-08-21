0c4d063a19bbd2e320d9f920ca81c5a7042af164
Daniel Ruswick <danielruswick@Daniels-MacBook-Air.local>
2021-08-20 23:43:17 -0500

    verified and delete data for me page

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,10 +1,17 @@
 import { userContext } from "../../context/context";
 import { useContext } from "react";
+import styles from "../../styles/Me.module.css";
 
 export default function Home() {
   const { user } = useContext(userContext);
   if (!user?.email) {
     return <h1>Not Logged In</h1>;
   }
-  return <h2>Hello {user.email}</h2>;
+  return (
+    <>
+      <h2>Hello {user.email}</h2>
+      {user.confirmed ? null : <h3>Your email is unconfirmed</h3>}
+      <button className="delete-my-data">Delete My Account and All Data</button>
+    </>
+  );
 }
