b82090f58c63aa844bc9c6c992782b94b3598a7d
Daniel Ruswick <danielruswick@Daniels-MacBook-Air.local>
2021-08-20 23:52:21 -0500

    added log out and delete styles

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,6 +1,7 @@
 import { userContext } from "../../context/context";
 import { useContext } from "react";
 import styles from "../../styles/Me.module.css";
+import { Link } from "next/link";
 
 export default function Home() {
   const { user } = useContext(userContext);
@@ -11,6 +12,9 @@ export default function Home() {
     <>
       <h2>Hello {user.email}</h2>
       {user.confirmed ? null : <h3>Your email is unconfirmed</h3>}
+      <Link>
+        <a>Log Out</a>
+      </Link>
       <button className={styles.deleteMyData}>
         Delete My Account and All Data
       </button>
