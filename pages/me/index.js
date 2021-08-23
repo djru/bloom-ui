1c74b08a94d8a9c85cb99647192dd2a655b03718
Daniel Ruswick <danielruswick@Daniels-MacBook-Air.local>
2021-08-22 21:59:07 -0500

    trying somethin

--- pages/me/index.js
+++ pages/me/index.js
@@ -12,9 +12,7 @@ export default function Home() {
     <>
       <h2>Hello {user?.email}</h2>
       {user?.confirmed ? null : <h3>Your email is unconfirmed</h3>}
-      <Link href="api.bloomhealth.app/logout">
-        <a>Log Out</a>
-      </Link>
+      <Link href="https://api.bloomhealth.app/logout">Log Out</Link>
       <button className={styles.deleteMyData}>
         Delete My Account and All Data
       </button>
