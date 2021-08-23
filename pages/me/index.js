14b986414b2d9ee118916494cf39a3f0bfa67062
Daniel Ruswick <danielruswick@Daniels-MacBook-Air.local>
2021-08-22 21:56:17 -0500

    fixed me page

--- pages/me/index.js
+++ pages/me/index.js
@@ -10,9 +10,9 @@ export default function Home() {
   }
   return (
     <>
-      <h2>Hello {user.email}</h2>
-      {user.confirmed ? null : <h3>Your email is unconfirmed</h3>}
-      <Link href="#">
+      <h2>Hello {user?.email}</h2>
+      {user?.confirmed ? null : <h3>Your email is unconfirmed</h3>}
+      <Link href="api.bloomhealth.app/logout">
         <a>Log Out</a>
       </Link>
       <button className={styles.deleteMyData}>
