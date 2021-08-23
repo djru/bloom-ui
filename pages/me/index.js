936c961ae8509aee179729fdb4e73929701eb379
Daniel Ruswick <danielruswick@Daniels-MacBook-Air.local>
2021-08-22 22:09:36 -0500

    fixed me

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,21 +1,30 @@
 import { userContext } from "../../context/context";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import styles from "../../styles/Me.module.css";
-import { Link } from "next/link";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const { user } = useContext(userContext);
-  if (!user?.email) {
-    return <h1>Not Logged In</h1>;
-  }
+  const { user, setErr } = useContext(userContext);
+  const router = useRouter();
+  useEffect(() => {
+    if (!user?.email) {
+      setErr("You are not logged in");
+      router.push("/");
+    }
+  }, [router, setErr, user]);
+
   return (
     <>
       <h2>Hello {user?.email}</h2>
       {user?.confirmed ? null : <h3>Your email is unconfirmed</h3>}
-      <Link href="https://api.bloomhealth.app/logout">Log Out</Link>
+      <Link href="https://api.bloomhealth.app/logout">
+        <a>Log Out</a>
+      </Link>
       <button className={styles.deleteMyData}>
         Delete My Account and All Data
       </button>
     </>
   );
+  return <></>;
 }
